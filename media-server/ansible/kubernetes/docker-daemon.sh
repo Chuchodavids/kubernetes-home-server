98231abf0555c50ac2cdc900f63c7db9a4332cab
chuchodavids <chuchodavids@hotmail.com>
2021-03-13 13:20:56 -0600

    added docker-daemon script. Not working for now.

---
+++ media-server/ansible/kubernetes/docker-daemon.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+FILE="/etc/docker/daemon.json"
+
+cat > $FILE << EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
